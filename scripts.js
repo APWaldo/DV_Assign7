c7f1519cb146ffa8fcafa9a6445de7da8d18e1fd
APWaldo <apw2131@columbia.edu>
2014-04-06 15:02:00 -0400

    Adding image array.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
 /**
  * @author Annie P Waldman
  */
+
+/*
+ * First I need to create an array of the images (they have all been resized to match each other).
+ */
+var images = [
+"1_8thAve-A1.jpg",
+"2_8thAve-A2.jpg",
+"3_BowlingGreen1.jpg",
+"4_BowlingGreen2.jpg",
+"5_delancy-1.jpg",
+"6_delancy-2.jpg",
+"7_mottB-1.jpg",
+"8_mottB-2.jpg",
+"9_Watchtower-1.jpg",
+"10_Watchtower-2.jpg"
+];
